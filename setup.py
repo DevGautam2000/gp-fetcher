da632fc7a08f91c8ee25ca76412c0f36c6af8a46
Gautam <devgautam1231@gmail.com>
2021-06-19 18:48:28 +0530

    added docs to pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
+here = os.path.abspath(os.path.dirname(__file__))
+long_description = ""
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 VERSION = '0.2'
-DESCRIPTION = 'A python package that fetches your public projects(repos) from github'
-LONG_DESCRIPTION = 'A python package that fetches your public projects(repos) from github repositories and generates a JSON file.'
+DESCRIPTION = 'A python package that fetches your public projects(repos) details from github'
 
 # Setting up
 setup(
@@ -13,7 +18,7 @@ setup(
     author_email="devgautam1231@gmail.com",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     packages=find_packages(),
     install_requires=['bs4','requests','tqdm','lxml'],
     keywords=['python', 'github', 'projects', 'repositories' , 'JSON'],
